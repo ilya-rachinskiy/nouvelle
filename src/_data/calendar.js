3b776ef75b4b2b2632b2d5f141a9f7498b62454e
ilya <ilya.ratchinsky@gmail.com>
2019-11-22 12:35:49 +0300

    Исправляет ошибки форматирования

--- src/_data/calendar.js
+++ src/_data/calendar.js
@@ -3,25 +3,26 @@ const calendar = 'https://web-standards.ru/calendar.json';
 
 function getJson(url) {
     return new Promise((resolve, reject) => {
-        https.get(url, (res) => {
-            let body = '';
+        https
+            .get(url, res => {
+                let body = '';
 
-            res.on('data', (chunk) => {
-                body += chunk;
+                res.on('data', chunk => {
+                    body += chunk;
+                });
+
+                res.on('end', () => {
+                    try {
+                        let response = JSON.parse(body);
+                        resolve(response);
+                    } catch (error) {
+                        reject(error);
+                    }
+                });
+            })
+            .on('error', e => {
+                reject(e);
             });
-
-            res.on('end', () => {
-                try {
-                    let response = JSON.parse(body);
-                    resolve(response);
-                } catch (error) {
-                    reject(error);
-                }
-            }
-            );
-        }).on('error', (e) => {
-            reject(e)
-        });
     });
 }
 
