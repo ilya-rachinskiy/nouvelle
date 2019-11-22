07e99f535ad67af3b3ad968bfa84cac59992bf63
Vadim Makeev <pepelsbey@gmail.com>
2019-11-22 11:23:54 +0300

    Правит форматирование и выносит адрес в константу

--- src/_data/calendar.js
+++ src/_data/calendar.js
@@ -1,27 +1,28 @@
 const https = require('https');
+const calendar = 'https://web-standards.ru/calendar.json';
 
 function getJson(url) {
-  return new Promise((resolve, reject) => {
-    https.get(url, (res) => {
-      let body = '';
+    return new Promise((resolve, reject) => {
+        https.get(url, (res) => {
+            let body = '';
 
-      res.on('data', (chunk) => {
-        body += chunk;
-      });
+            res.on('data', (chunk) => {
+                body += chunk;
+            });
 
-      res.on('end', () => {
-        try {
-          let response = JSON.parse(body);
-          resolve(response);
-        } catch (error) {
-          reject(error);
-        }
-      }
-      );
-    }).on('error', (e) => {
-      reject(e)
+            res.on('end', () => {
+                try {
+                    let response = JSON.parse(body);
+                    resolve(response);
+                } catch (error) {
+                    reject(error);
+                }
+            }
+            );
+        }).on('error', (e) => {
+            reject(e)
+        });
     });
-  });
 }
 
-module.exports = () => getJson('https://web-standards.ru/calendar.json').then(events => ({ events }));
+module.exports = () => getJson(calendar).then(events => ({ events }));
